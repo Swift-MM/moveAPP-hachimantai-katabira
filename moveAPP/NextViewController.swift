21aabae0f6e80aa70b6fd0d93080cb7d44ee2de3
katabira <ludmata.2@gmail.com>
2016-10-30 18:07:51 +0900

    add

---
+++ moveAPP/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  moveAPP
+//
+//  Created by Arnie on 2016/10/30.
+//  Copyright © 2016年 Arnie. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
